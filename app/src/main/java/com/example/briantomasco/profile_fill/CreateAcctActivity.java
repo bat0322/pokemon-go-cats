eb83593254acdf849154ded0ef9358fa15584997
zjohnson5455 <Zachary.D.Johnson.20@dartmouth.edu>
2017-10-23 17:06:12 -0400

    Testing VCS connection #3

--- app/src/main/java/com/example/briantomasco/profile_fill/CreateAcctActivity.java
+++ app/src/main/java/com/example/briantomasco/profile_fill/CreateAcctActivity.java
@@ -7,8 +7,6 @@ package com.example.briantomasco.profile_fill;
  * Saves entered data to server and signs user in
  */
 
-// TESTING TESTING
-
 import android.Manifest;
 import android.content.ContentValues;
 import android.content.Context;
